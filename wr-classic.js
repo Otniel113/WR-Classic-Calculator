32d3c71611f9456573d60271eb449a41ca5ee975
Otniel113 <otniel.abiezer.saragih@gmail.com>
2022-10-26 11:33:40 +0700

    Mengubah sedikit di jquery

--- wr-classic.js
+++ wr-classic.js
@@ -30,6 +30,6 @@ function hitungWR(wr_total, match_total, wr_ranked, match_ranked){
     return ("Anda Memiliki WR Classic = " + wr_classic.toFixed(2) + "% dengan banyak match = " + match_classic);
 }
 
-$("ValidatorInput").submit(function(event) {
+$("form").submit(function(event) {
     event.preventDefault();
 });
